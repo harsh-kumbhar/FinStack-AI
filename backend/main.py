f1e468c41c24ba76749f1edb0868a4e8e8d3d5e8
YashVekhande <yashsvekhande@gmail.com>
2026-09-09 14:46:08 +0530

    Main.py

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from modules.financial_health.router import router as financial_health_router
 from modules.smartfeed.router import router as smartfeed_router
 from modules.tax_estimator.router import router as tax_estimator_router
+from modules.loan_risk_assessment.router import router as loan_risk_assessment_router
 
 app = FastAPI(
     title="FinStack API",
@@ -22,3 +23,4 @@ app.add_middleware(
 app.include_router(financial_health_router)
 app.include_router(smartfeed_router)
 app.include_router(tax_estimator_router)
+app.include_router(loan_risk_assessment_router)
